8620864406dfa660f94b99f4ca0c07f51cd50dd8
gitubjimmy <jimmynew1130@gmail.com>
2021-02-20 22:00:55 +0900

    cron job succeded

--- WebApp/var_table/apps.py
+++ WebApp/var_table/apps.py
@@ -3,8 +3,8 @@ import os
 
 class VarTableConfig(AppConfig):
     name = 'var_table'
-
     def ready(self):
         from . import cron
+
         if os.environ.get('RUN_MAIN', None) != 'true':
             cron.start_scheduler()
--- WebApp/var_table/cron.py
+++ WebApp/var_table/cron.py
@@ -1,8 +1,9 @@
 from django.db import connection
 from .models import Variables
-from schedule import Scheduler
-import time
+import sys
+import schedule
 import threading
+import time
 
 def refresh_variables():
     with connection.cursor() as cursor:
@@ -13,36 +14,37 @@ def refresh_variables():
             var_model = Variables(name=var[0], value=var[1])
             var_model.save()
 
-def run_continuously(self, interval=1):
-    """Continuously run, while executing pending jobs at each elapsed
-    time interval.
-    @return cease_continuous_run: threading.Event which can be set to
-    cease continuous run.
-    Please note that it is *intended behavior that run_continuously()
-    does not run missed jobs*. For example, if you've registered a job
-    that should run every minute and you set a continuous run interval
-    of one hour then your job won't be run 60 times at each interval but
-    only once.
+def run_continuously(interval=1):
+    """Continuously run, while executing pending jobs at each
+    elapsed time interval.
+    @return cease_continuous_run: threading. Event which can
+    be set to cease continuous run. Please note that it is
+    *intended behavior that run_continuously() does not run
+    missed jobs*. For example, if you've registered a job that
+    should run every minute and you set a continuous run
+    interval of one hour then your job won't be run 60 times
+    at each interval but only once.
     """
+    try:
+        cease_continuous_run = threading.Event()
 
-    cease_continuous_run = threading.Event()
+        class ScheduleThread(threading.Thread):
+            @classmethod
+            def run(cls):
+                while not cease_continuous_run.is_set():
+                    schedule.run_pending()
+                    time.sleep(interval)
 
-    class ScheduleThread(threading.Thread):
-
-        @classmethod
-        def run(cls):
-            while not cease_continuous_run.is_set():
-                self.run_pending()
-                time.sleep(interval)
-
-    continuous_thread = ScheduleThread()
-    continuous_thread.setDaemon(True)
-    continuous_thread.start()
-    return cease_continuous_run
-
-Scheduler.run_continuously = run_continuously
+        continuous_thread = ScheduleThread()
+        continuous_thread.start()
+        return cease_continuous_run
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
 
 def start_scheduler():
-    scheduler = Scheduler()
     schedule.every(1).minutes.do(refresh_variables)
-    scheduler.run_continuously()
+    stop_run_continuously = run_continuously()
