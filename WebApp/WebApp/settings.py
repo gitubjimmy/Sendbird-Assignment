90cefc58651d82702958cd063adb02458325578e
gitubjimmy <jimmynew1130@gmail.com>
2021-02-20 21:16:32 +0900

    cron-schedule

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -37,8 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'var_table',
-    'django_crontab',
+    'var_table.apps.VarTableConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,8 +121,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-# Cron Jobs
-CRONJOBS = [
-    ('*/1 * * * *', 'var_table.cron.refresh_variables')
-]
--- WebApp/var_table/apps.py
+++ WebApp/var_table/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
-
+import os
 
 class VarTableConfig(AppConfig):
     name = 'var_table'
+
+    def ready(self):
+        from . import cron
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            cron.start_scheduler()
